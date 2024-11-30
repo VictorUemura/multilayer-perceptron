4eea3756f8bb6c429b697fce9a04f232c596df9a
Victor Hiroshi Uemura <103529158+VictorUemura@users.noreply.github.com>
2024-11-30 20:09:13 -0300

    feat: classe onde pode ser definido e atualizada a funcao de ativacao utilizada pelos neuronios

--- script.js
+++ script.js
@@ -1,3 +1,60 @@
+// cada neuronio possui uma funcao de ativacao que eh definida pelo usuario
+class FuncoesDeAtivacao {
+    constructor(tipo = 'linear') {
+        if(tipo.toLowerCase() === 'linear') {
+            this.funcao = (net) => {
+                return (net * 1.0) / 10;
+            }
+            this.derivada = (net) => {
+                return 1.0 / 10;
+            }
+        }
+        else if(tipo.toLowerCase() === 'logistica') {
+            this.funcao = (net) => {
+                return 1.0 / (1 + Math.exp(-net));
+            }
+            this.derivada = (net) => {
+                return this.funcao(net) * (1 - this.funcao(net));
+            }
+        }
+        else if(tipo.toLowerCase() === 'tangentehiperbolica') {
+            this.funcao = (net) => {
+                return Math.tanh(net);
+            }
+            this.derivada = (net) => {
+                return 1 - Math.pow(this.funcao(net), 2);
+            }
+        }
+    }
+
+    update(tipo = 'linear') {
+    if(tipo.toLowerCase() === 'linear') {
+        this.funcao = (net) => {
+            return (net * 1.0) / 10;
+        }
+        this.derivada = (net) => {
+            return 1.0 / 10;
+        }
+    }
+    else if(tipo.toLowerCase() === 'logistica') {
+        this.funcao = (net) => {
+            return 1.0 / (1 + Math.exp(-net));
+        }
+        this.derivada = (net) => {
+            return this.funcao(net) * (1 - this.funcao(net));
+        }
+    }
+    else if(tipo.toLowerCase() === 'tangentehiperbolica') {
+        this.funcao = (net) => {
+            return Math.tanh(net);
+        }
+        this.derivada = (net) => {
+            return 1 - Math.pow(this.funcao(net), 2);
+        }
+    }
+    }
+}
+
 const camadaDeEntradas = []; // aqui respectivamente irão todos os valores de entrada recebidos em um arquivo .csv
 const camadaDeNeuronios = []; // neste array é definido a quantidade de perceptrons de acordo com a quantidade definida
 const camadaDeSaida = []; // neste array é definido os perceptrons que retornam alguma saida
