ea85ab82725353eee6ec749b1eb02af35b67be6f
Victor Hiroshi Uemura <103529158+VictorUemura@users.noreply.github.com>
2024-11-30 20:08:28 -0300

    feat: ideia da estrutura de neuronios de funcionamento

---
+++ script.js
@@ -0,0 +1,3 @@
+const camadaDeEntradas = []; // aqui respectivamente irão todos os valores de entrada recebidos em um arquivo .csv
+const camadaDeNeuronios = []; // neste array é definido a quantidade de perceptrons de acordo com a quantidade definida
+const camadaDeSaida = []; // neste array é definido os perceptrons que retornam alguma saida
